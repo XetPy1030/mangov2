748db69e53f024f1467f9f0f8b75d76ebddb9ea9
XetPy1030 <xetpy1030@mail.ru>
2023-04-17 02:09:47 +0300

    Comm

--- handlers/test/image.py
+++ handlers/test/image.py
@@ -8,3 +8,9 @@ from core import router
 async def get_photo_id(message: types.Message):
     await message.answer(message.photo[-1].file_id)
     print(message.photo[-1].file_id)
+
+
+@router.message()
+async def test(message: types.Message):
+    print(dir(message))
+    await message.answer('test')
