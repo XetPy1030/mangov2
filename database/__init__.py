0adef6575a13e547d22e1215d1315e097f2da006
XetPy1030 <xetpy1030@mail.ru>
2023-04-17 01:55:44 +0300

    Comm

--- database/__init__.py
+++ database/__init__.py
@@ -1,21 +1,5 @@
+from .configure import configure_app
+
+configure_app()
+
 from .models import *
-
-
-def start():
-    import os
-
-    import django
-    from django.conf import settings
-
-    from . import settings as settings_default
-
-    os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
-
-    settings.configure(default_settings=settings_default, DEBUG=True)
-
-    django.setup()
-
-    from django.core.management import call_command
-
-    call_command('makemigrations', 'database')
-    call_command('migrate')
--- main.py
+++ main.py
@@ -12,8 +12,6 @@ async def run_bot():
 if __name__ == '__main__':
     import database
 
-    database.start()
-
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     loop.run_until_complete(run_bot())
