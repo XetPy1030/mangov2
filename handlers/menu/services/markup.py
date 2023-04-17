b47d920317bb67f971e37b54707174d302499aa6
XetPy1030 <xetpy1030@mail.ru>
2023-04-18 02:18:36 +0300

    Comm

--- handlers/menu/services/markup.py
+++ handlers/menu/services/markup.py
@@ -11,13 +11,25 @@ def get_markup_services(page: int = 0):
             InlineKeyboardButton(text=service.name, callback_data=f'service:{service.__class__.__name__}')
         )
 
+    actions_keyboard = []
+    if page > 0:
+        actions_keyboard.append(InlineKeyboardButton(text='Назад', callback_data=f'services_prev:{page}'))
+    else:
+        actions_keyboard.append(InlineKeyboardButton(text='Назад', callback_data=''))
+
+    actions_keyboard.append(InlineKeyboardButton(text=f'{page + 1}/{len(services) // PER_PAGE + 1}', callback_data=''))
+
+    if (page + 1) * PER_PAGE < len(services):
+        actions_keyboard.append(InlineKeyboardButton(text='Вперед', callback_data=f'services_next:{page}'))
+    else:
+        actions_keyboard.append(InlineKeyboardButton(text='Вперед', callback_data=''))
+
     keyboard = [
         *[
             [i] for i in services_keyboard
         ],
         [
-            InlineKeyboardButton(text='Назад', callback_data=f'services_prev:{page}'),
-            InlineKeyboardButton(text='Вперед', callback_data=f'services_next:{page}'),
+            *actions_keyboard
         ]
     ]
 
