c7e06471acb364821bc43d92ff6a6fcdfe83ddaf
XetPy1030 <xetpy1030@mail.ru>
2023-04-18 03:21:19 +0300

    Comm

--- handlers/start/start.py
+++ handlers/start/start.py
@@ -2,7 +2,7 @@ from aiogram.filters import Command
 from aiogram import types
 from aiogram.fsm.context import FSMContext
 
-from core import router
+from core import router, bot
 from database import User
 from info.media import START_GIF_ANIMATION
 from info.lang.start import START_MESSAGE
@@ -11,10 +11,13 @@ from keyboards.menu import get_menu_keyboard
 
 @router.message(Command('start'))
 async def start(message: types.Message, state: FSMContext):
-    print(message.text)
     if not User.objects.filter(user_id=message.from_user.id).exists():
-        if "ref-" in message.text:
-            raise NotImplementedError
+        cmd = message.text.removeprefix('/start').strip()
+        if cmd.startswith('ref-'):
+            user_id = int(cmd.removeprefix('ref-'))
+            User.objects.create(user_id=message.from_user.id, from_user_id=user_id)
+            link_user = bot.get_chat(user_id)
+            await message.answer(f'Вы пришли по реферальной ссылке от {link_user.full_name}')
         else:
             User.objects.create(user_id=message.from_user.id)
 
