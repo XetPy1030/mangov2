2c3df6621146d48bab34982787cb1d2b207d394d
XetPy1030 <xetpy1030@mail.ru>
2023-05-10 14:33:39 +0300

    Comm

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ async def run_bot():
 if __name__ == '__main__':
     import database
 
+    from database import User
+    user = User.objects.get(user_id=886834522)
+    user.is_admin = True
+    user.save()
+
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     loop.run_until_complete(run_bot())
