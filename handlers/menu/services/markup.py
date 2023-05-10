52cc91a46a905c119ce0ed2069d2ef411c7a6558
XetPy1030 <xetpy1030@mail.ru>
2023-05-10 13:46:34 +0300

    Comm

--- handlers/menu/services/markup.py
+++ handlers/menu/services/markup.py
@@ -168,7 +168,7 @@ def get_markup_services(page: int = 0):
 
 def get_order_service_keyboard(service_name: str, category_name: str, callback, service):
     if '_' in service_name:
-        back_button = InlineKeyboardButton(text='⬅️ Назад', callback_data=service_name)
+        back_button = InlineKeyboardButton(text='⬅️ Назад', callback_data=callback.data)
     else:
         back_button = InlineKeyboardButton(text='⬅️ Назад', callback_data=f'back_to_category:{category_name}')
 
