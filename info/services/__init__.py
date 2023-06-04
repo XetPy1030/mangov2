9f884aab9bd39b8149c62ec06b245cd4a1764939
XetPy1030 <xetpy1030@mail.ru>
2023-06-04 19:52:50 +0300

    sefef

--- info/services/__init__.py
+++ info/services/__init__.py
@@ -4,68 +4,70 @@ from aiogram import types
 
 from .services import *
 
-services_with_categories = {
-    'telegram': {
-        'name': 'Телеграм',
-        'services': [
-            ChannelPackaging(),
-            ChannelPromotionAdBlogers(),
-            ChannelPromotionAd(),
-            Logo(),
-            DesignBanners(),
+# services_with_categories = {
+#     'telegram': {
+#         'name': 'Телеграм',
+#         'services': [
+#             ChannelPackaging(),
+#             ChannelPromotionAdBlogers(),
+#             ChannelPromotionAd(),
+#             Logo(),
+#             DesignBanners(),
+#
+#             Bot(),
+#             AdvertisingCreative(),
+#             ContentChannel(),
+#             ChannelAdmin(),
+#
+#             ScreenwriterOnFunnel(),
+#             Monetization(),
+#         ],
+#         'type': 'simple',
+#     },
+#     'youtube': {
+#         'name': 'Youtube',
+#         'services': [
+#             AdvertisingManager(),
+#             Designer(),
+#             ContentDeveloper(),
+#             MontagerCreatorVideos(),
+#         ],
+#         'type': 'simple',
+#     },
+#     'web': {
+#         'name': 'Веб',
+#         'services': [
+#             UXUIDesign(),
+#             DevelopmentITPlatforms(),
+#             TildaGroup(),
+#         ],
+#         'type': 'simple',
+#     },
+#     'insta': {
+#         'name': 'Инстаграм',
+#         'services': [
+#             TargetInsta(),
+#             StoriesMakerInsta(),
+#             ContentManagerInsta(),
+#             VisualAssistantInsta(),
+#
+#             CopyWriterInsta(),
+#             ScenaristInsta(),
+#             ContentCreatorInsta(),
+#             RealsMaker(),
+#         ],
+#         'type': 'simple'
+#     },
+#     'other': {
+#         'name': 'Другое',
+#         'services': [
+#             Consultation(),
+#             ConsultationWithAgencyOwner(),
+#         ]
+#     }
+# }
 
-            Bot(),
-            AdvertisingCreative(),
-            ContentChannel(),
-            ChannelAdmin(),
-
-            ScreenwriterOnFunnel(),
-            Monetization(),
-        ],
-        'type': 'simple',
-    },
-    'youtube': {
-        'name': 'Youtube',
-        'services': [
-            AdvertisingManager(),
-            Designer(),
-            ContentDeveloper(),
-            MontagerCreatorVideos(),
-        ],
-        'type': 'simple',
-    },
-    'web': {
-        'name': 'Веб',
-        'services': [
-            UXUIDesign(),
-            DevelopmentITPlatforms(),
-            TildaGroup(),
-        ],
-        'type': 'simple',
-    },
-    'insta': {
-        'name': 'Инстаграм',
-        'services': [
-            TargetInsta(),
-            StoriesMakerInsta(),
-            ContentManagerInsta(),
-            VisualAssistantInsta(),
-
-            CopyWriterInsta(),
-            ScenaristInsta(),
-            ContentCreatorInsta(),
-            RealsMaker(),
-        ],
-        'type': 'simple'
-    },
-    'other': {
-        'name': 'Другое',
-        'services': [
-            Consultation(),
-            ConsultationWithAgencyOwner(),
-        ]
-    }
-}
+services_with_categories = {}
 
 texts = {
     'main_folder': """Добро пожаловать в Digital - агенство «BB$: Brain & Best solution»
