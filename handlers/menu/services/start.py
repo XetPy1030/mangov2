c34127a4d83bdb594a9550ef8db26031b0e4ee9b
XetPy1030 <xetpy1030@mail.ru>
2023-06-04 21:49:41 +0300

    sefef

--- handlers/menu/services/start.py
+++ handlers/menu/services/start.py
@@ -29,16 +29,19 @@ def handler_for_markup_for_service(markup: types.InlineKeyboardMarkup):
         for j in i:
             splitted_callback_data = j.callback_data.split(':')
             if len(splitted_callback_data) == 1:
-                page_index = int(splitted_callback_data[0].split('@')[1]) if len(splitted_callback_data[0].split('@')) > 1 else 0
-                data = splitted_callback_data[0]+'@'+str(page_index)
+                page_index = int(splitted_callback_data[0].split('@')[1]) if len(
+                    splitted_callback_data[0].split('@')
+                    ) > 1 else 0
+                data = splitted_callback_data[0] + '@' + str(page_index)
             elif len(splitted_callback_data) == 0:
                 data = ''
             else:
                 saved = splitted_callback_data[:-1]
-                page_index = int(splitted_callback_data[-1].split('@')[1]) if len(splitted_callback_data[-1].split('@')) > 1 else 0
-                data = ':'.join(saved)+':'+splitted_callback_data[-1]+'@'+str(page_index)
+                page_index = int(splitted_callback_data[-1].split('@')[1]) if len(
+                    splitted_callback_data[-1].split('@')
+                    ) > 1 else 0
+                data = ':'.join(saved) + ':' + splitted_callback_data[-1] + '@' + str(page_index)
                 print('han', splitted_callback_data, data, saved, page_index)
-
 
             j.callback_data = 'new_service:' + data
     return markup
@@ -54,15 +57,15 @@ async def our_services(message: Message):
 @router.callback_query(CallbackFilter('new_service'))
 async def new_service(call):
     callback_data = call.data.removeprefix('new_service:')
-    split_data = callback_data.split('@')
-    page_str = split_data[0]
+
+    page_str = ':'.join([i.split('@')[0] for i in callback_data.split(':')])
+
+    split_data = callback_data.split(':')[-1].split('@')
     page_index = int(split_data[1]) if len(split_data) > 1 else 0
     page_str_copy = call.data.removeprefix('new_service:')
-    print(page_str_copy, page_str)
     text, markup, image = render(page_str, page_index=page_index, page_str_copy=page_str_copy)
     markup = handler_for_markup_for_service(markup)
     await answer(call.message, text, markup, image)
-
 
 
 @router.callback_query(CallbackFilter('category'))
@@ -112,4 +115,3 @@ async def back_to_services(call):
         caption=lang.menu.services.OUR_SERVICES,
         reply_markup=render_services_from_category(category_name, int(category_page))
     )
-
